41a5b85bc3c49fdf3eebcd0d1e6b1051084d40f9
sps2604 <97956985+sps2604@users.noreply.github.com>
2025-09-17 18:06:31 +0530

    Update FindAJob.tsx

--- src/pages/FindAJob.tsx
+++ src/pages/FindAJob.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import  { useState, useEffect } from "react";
 import { useSearchParams, useNavigate } from "react-router-dom";
 import FilterSidebar from "../components/FilterSidebar";
 import AfterLoginNavbar from "../components/AfterLoginNavbar";
